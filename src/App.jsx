231a63816a0984ff3c3e7d021039b85f676bfc30
Mykhaylo Chuhayev <chugayev_misha@ukr.net>
2023-05-14 18:12:32 +0300

    Header was added as a component Style was modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Button from "./components/Button/Button.jsx";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
-  return (
-      <div>
-        <h1>Some useless buttons</h1>
-        <Button text={"SUBMIT"} myClass={"button btn-submit"}></Button>
-        <Button text={"RESET"} myClass={"button btn-reset"}></Button>
-      </div>
-  );
+    return (
+        <div>
+            <Header text={"Couple of useless buttons"}></Header>
+            <div className={"buttons"}>
+                <Button text={"SUBMIT"} myClass={"button btn-submit"}></Button>
+                <Button text={"RESET"} myClass={"button btn-reset"}></Button>
+            </div>
+        </div>
+    );
 }
 
 export default App
