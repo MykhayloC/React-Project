971162fde567629ac32c519a1acb77abce4a7b46
Mykhaylo Chuhayev <chugayev_misha@ukr.net>
2023-05-30 17:53:00 +0300

    hw-2 is done

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,41 +1,32 @@
-import {useState, useEffect} from "react";
-import './Users.css';
+import {useEffect, useState} from "react";
+import {Link} from 'react-router-dom';
+import './Users.css'
 
-function Users() {
+const Users = () => {
     const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState("");
+    const [error, setError] = useState('');
 
     useEffect(() => {
-
-
-        const getUsers = async () => {
+        const fetchUsers = async () => {
             try {
-                setIsLoading(true)
                 const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
                 if (!response.ok) {
-                    throw new Error ('HTTP error')
+                    throw new Error('HTTP-request error!')
                 }
 
                 const data = await response.json();
                 setUsers(data);
             } catch (e) {
-                setError(e.message)
-            } finally {
-                setIsLoading(false)
+                setError(e.message);
             }
         }
-        getUsers();
+        fetchUsers();
     }, [])
 
     return (
         <div>
-            {isLoading &&
-                <div className="spinner-border text-info" role="status">
-                    <span className="sr-only">Loading...</span>
-                </div>
-            }
+            <h2>Users</h2>
             {error &&
                 <div>
                     <h2 className={"error-header"}>Oops!..</h2>
@@ -44,10 +35,8 @@ function Users() {
             }
             {users && users.map(user =>
                 <div className={"user"} key={user.id}>
-                    <h6 className={"user-header"} >Name: {user.name}</h6>
+                    <Link to={`/users/${user.id}`} className={"user-header"} >Name: {user.name}</Link>
                     <p className={"user-body"}>Email: {user.email}</p>
-                    <p className={"user-body"}>Phone-number: {user.phone}</p>
-                    <p className={"user-body"}>City: {user.address.city}</p>
                     <p className={"user-body"}>Company: {user.company.name}</p>
                 </div>
             )}
